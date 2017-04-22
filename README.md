a17150f123aae78f5bd9fe70e9a0b8b923ace190
Aaron English <Aaron.H.English@gmail.com>
2017-04-22 10:12:17 -0500

    First Commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Todo List Application
 =====================
 
 Eyyyyyyyy, everybody needs a todo list application, don't they?
+# OK-Coders-Todo-List-App
---
+++ index.js
@@ -0,0 +1,29 @@
+var restify = require('restify');
+var mongoose = require('mongoose');
+var server = restify.createServer();
+
+cnost port = 27017;
+const dbserver = 'mongodb://localhost/todo'
+
+var tasks = require('./routes/tasks');
+
+mongoose.connect(dbserver);
+var db = mongoose.connection;
+
+db.on('error', function(msg) {
+  console.log('Mongoose bit the dust; ' + msg);
+});
+
+db.once('open', function() {
+  console.log("Mongoose connection established.");
+});
+
+server.get('/', function(req, res, next) {
+  res.send("SUCCESS");
+});
+
+server.get('/tasks', tasks.read);
+
+server.listen(port, function() {
+  console.log('%s listening on %s', server.name, port);
+});
