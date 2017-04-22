822fa3254d1e5c4d9d8c1201005662aee91f0490
Aaron English <Aaron.H.English@gmail.com>
2017-04-22 11:58:03 -0500

    First commit

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var restify = require('restify');
 var mongoose = require('mongoose');
 var server = restify.createServer();
 
-cnost port = 27017;
+cnost port = 8088;
 const dbserver = 'mongodb://localhost/todo'
 
 var tasks = require('./routes/tasks');
+var client = require('./client');
 
 mongoose.connect(dbserver);
 var db = mongoose.connection;
@@ -18,11 +19,9 @@ db.once('open', function() {
   console.log("Mongoose connection established.");
 });
 
-server.get('/', function(req, res, next) {
-  res.send("SUCCESS");
-});
-
+server.get('/', client.get);
 server.get('/tasks', tasks.read);
+server.post('/tasks/:name', tasks.create);
 
 server.listen(port, function() {
   console.log('%s listening on %s', server.name, port);
--- routes/tasks/index.js
+++ routes/tasks/index.js
@@ -7,3 +7,17 @@ exports.read = function(req, res, next) {
   });
   return next();
 }
+
+exports.create = function(req,res,next) {
+  var task = new Tasks();
+
+  task.task = req.params.task;
+  task.date = new Date();
+  task.completed = false;
+
+  task.save(function(err, data) {
+    if(err){ console.log("Error saving to db: " + err); }
+    else { res.json( {result: "success", id: data._id} ); }
+  });
+  return next();
+}
